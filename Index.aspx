b3f6072a9267862c74ce048bef5db28c5ec36180
Renato Carvalho <renatoluizcarvalho@gmail.com>
2015-10-09 14:48:45 -0300

    Example Project

---
+++ Index.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="XMLSerialize.Index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button Text="Serialize" ID="btnSerialize" runat="server" OnClick="btnSerialize_Click" />
+            <asp:Button Text="Deserialize" ID="btnDeserialize" runat="server" OnClick="btnDeserialize_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Index.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Serialization;
+
+namespace XMLSerialize
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            /*No code here*/
+        }
+
+        protected void btnSerialize_Click(object sender, EventArgs e)
+        {
+            var objShipOrder = new XMLClass.shiporder();
+            var objShipOrderItem = new XMLClass.shiporderItem();
+            var objShipTo = new XMLClass.shiporderShipto();
+
+            objShipTo.address = "Address Value";
+            objShipTo.city = "City Value";
+            objShipTo.country = "Country Value";
+            objShipTo.name = "Name Value";
+
+            objShipOrderItem.title = "Title Value";
+            objShipOrderItem.quantity = "0";
+            objShipOrderItem.price = 0;
+            objShipOrderItem.note = "Note Value";
+
+            XMLClass.shiporderItem[] shipOrderItens = new XMLClass.shiporderItem[1];
+            shipOrderItens[0] = objShipOrderItem;
+
+            objShipOrder.item = shipOrderItens;
+            objShipOrder.shipto = objShipTo;
+
+            objShipOrder.orderid = "0";
+            objShipOrder.orderperson = "Person Value";
+
+            //Serialize
+            XmlSerializer serializer = new XmlSerializer(typeof(XMLClass.shiporder));
+            serializer.Serialize(File.Create("C:\\XML\\OrderShip.xml"), objShipOrder);
+        }
+
+        protected void btnDeserialize_Click(object sender, EventArgs e)
+        {
+            var objShipOrder = new XMLClass.shiporder();
+
+            //Serialize
+            XmlSerializer serializer = new XmlSerializer(typeof(XMLClass.shiporder));
+
+            StreamReader stReader = new StreamReader("C:\\XML\\OrderShip.xml");
+            objShipOrder = (XMLClass.shiporder)serializer.Deserialize(stReader);
+            stReader.Close();
+        }
+    }
+}
